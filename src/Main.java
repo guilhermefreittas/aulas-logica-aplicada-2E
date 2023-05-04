f7b66c8318df8fec49095f4fd592ddd3ba6fba7c
guilhermefreittas <gui.fernandes242020@gmail.com>
2023-05-04 11:55:00 -0300

    Aula de Introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        //laço enquanto - while
-        int contador = 0;
-        while(contador < 10){
-            System.out.println("contador= "+contador);
-            //incremento
-            contador = contador + 1; //ou contador++
-        }
+        //aula introdutória de Orientação à Objetos
 
-        //laço do para faça - for
-        for(int x =0; x<10; x++){
-            System.out.println("contador =" +x);
-        }
 
-        //faça faça enquanto - do while
-        int cont =1;
-        do{
-            //processo
-            System.out.println(cont);
-            cont++; //incremento
-        }while(cont <= 10);
-    }
+
+
+
+
+
+            }
 }
